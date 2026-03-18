824723438bf2263157df1542e96c95a762e7968d
SanketTops <sanket.tops@gmail.com>
2026-03-18 14:16:12 +0530

    mod-1

---
+++ Assignments/Module-1/cast.py
@@ -0,0 +1,7 @@
+"""x='54'
+y='76'
+print(int(x)+int(y))"""
+
+x=46.67532
+print(x)
+print(int(x))
---
+++ Assignments/Module-1/comment.py
@@ -0,0 +1,4 @@
+#print("Hello Python!")
+
+'''print("Good Afternoon")
+print("Hello Students, This is Python!")'''
