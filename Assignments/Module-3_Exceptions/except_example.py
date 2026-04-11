60d42bf966f7195f63c83bae31dcb2d7435aa600
SanketTops <sanket.tops@gmail.com>
2026-04-11 14:39:14 +0530

    Exceptions

---
+++ Assignments/Module-3_Exceptions/except_example.py
@@ -0,0 +1,7 @@
+a=45
+b=65
+print("Sum:",A+b)
+
+x=56
+y=87
+print("Production:",x*y)
---
+++ Assignments/Module-3_Exceptions/try_except.py
@@ -0,0 +1,11 @@
+a=45
+b=65
+
+try:
+    print("Sum:",A+b)
+except:
+    print("Error")
+
+x=56
+y=87
+print("Production:",x*y)
