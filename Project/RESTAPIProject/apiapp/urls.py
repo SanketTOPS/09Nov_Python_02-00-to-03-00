8ecfc4c9387e4f5971e4a398b09272bc97a539ce
SanketTops <sanket.tops@gmail.com>
2026-05-25 14:43:57 +0530

    REST API

--- Project/RESTAPIProject/apiapp/urls.py
+++ Project/RESTAPIProject/apiapp/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('getdata/',views.getdata),
     path('searchdata/<int:id>',views.searchdata),
     path('deletedata/<int:id>',views.deletedata),
+    path('savedata/',views.savedata),
+    path('updatedata/<int:id>',views.updatedata),
+    path('',views.index),
 ]
--- Project/RESTAPIProject/apiapp/views.py
+++ Project/RESTAPIProject/apiapp/views.py
@@ -4,6 +4,7 @@ from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import requests
 
 # Create your views here.
 @api_view(['GET'])
@@ -36,4 +37,40 @@ def deletedata(request,id):
         Studinfo.delete(stid)
         return Response(status=status.HTTP_202_ACCEPTED)
         
+    
+@api_view(['POST'])
+def savedata(request):
+    if request.method=='POST':
+        serial=StudSerial(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+@api_view(['PUT','GET'])
+def updatedata(request,id):
+    try:
+        stid=Studinfo.objects.get(id=id)
+    except Studinfo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET':
+        serial=StudSerial(stid)
+        return Response(data=serial.data)
+    if request.method=='PUT':
+        serial=StudSerial(data=request.data,instance=stid)
+        if serial.is_valid():
+            serial.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+
+def index(request):
+    url="http://127.0.0.1:8000/getdata/"
+    req=requests.get(url)
+    data=req.json()
+    print(data)
+    return render(request,'index.html',{'data':data})
+    
     
